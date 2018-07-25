70b52dab38b289f8c1864ea548885dc48be32258
Patryk Pilek <patryk.pilek@gmail.com>
2018-07-25 18:53:57 +0100

    Connect app to NASA API.

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Search from './views/Search.vue';
 import About from './views/About.vue';
 
 Vue.use(Router);
@@ -10,7 +10,7 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home,
+      component: Search,
     },
     {
       path: '/about',
